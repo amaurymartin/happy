8d14f68b8d5c6b16d5914e1511d163df275c8e59
Amaury Martin <amaury.conde@hotmail.com>
2021-09-04 17:50:38 -0300

    Improve home page on frontend web

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -1,8 +1,29 @@
+import { Link } from 'react-router-dom';
+import { FiArrowRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/images/logo.svg';
+
 import './styles.css';
 
 const Home: React.FC = () => (
-  <div className="home">
-    <h1>Home</h1>
+  <div id="home">
+    <div className="content-wrapper">
+      <img src={logoImg} alt="happy" />
+
+      <main>
+        <h1>Bring happiness to the world</h1>
+        <p>Visit orphanages and change the day for many children</p>
+      </main>
+
+      <div className="location">
+        <strong>Fortaleza</strong>
+        <span>Ceará</span>
+      </div>
+
+      <Link to="/orphanages" className="map-link">
+        <FiArrowRight size={26} color="rbga(0, 0, 0, 0.6)" />
+      </Link>
+    </div>
   </div>
 );
 
