f1f258697a1495399096b74cf2194dcabdc5731d
Amaury Martin <amaury.conde@hotmail.com>
2021-09-04 18:09:16 -0300

    Improve orphanagesIndex page on frontend web

--- web/src/pages/orphanages/index/index.tsx
+++ web/src/pages/orphanages/index/index.tsx
@@ -1,8 +1,31 @@
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import mapMarkerImg from '../../../assets/images/map-marker.svg';
+
 import './styles.css';
 
 const OrphanagesIndex: React.FC = () => (
   <div id="orphanages-index">
-    <h1>OrphanagesIndex</h1>
+    <aside>
+      <header>
+        <img src={mapMarkerImg} alt="happy" />
+
+        <h2>Choose an orphanage on the map</h2>
+        <p>Many children are waiting for your visit</p>
+      </header>
+
+      <footer>
+        <strong>Fortaleza</strong>
+        <span>Ceará</span>
+      </footer>
+
+      <div />
+
+      <Link to="/orphanages/new" className="new-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </aside>
   </div>
 );
 
