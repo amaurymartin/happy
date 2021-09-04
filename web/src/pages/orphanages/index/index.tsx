7763d2c669aecc06437f16936772d6a2787968b3
Amaury Martin <amaury.conde@hotmail.com>
2021-09-04 17:55:42 -0300

    Init orphanagesIndex page on frontend web

---
+++ web/src/pages/orphanages/index/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css';
+
+const OrphanagesIndex: React.FC = () => (
+  <div id="orphanages-index">
+    <h1>OrphanagesIndex</h1>
+  </div>
+);
+
+export default OrphanagesIndex;
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from './pages/home';
+import OrphanagesIndex from './pages/orphanages/index';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
-    <Route component={Home} path="/" exact />
+    <Route path="/" exact component={Home} />
+    <Route path="/orphanages" component={OrphanagesIndex} />
   </BrowserRouter>
 );
 
