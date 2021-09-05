7a24984e7f0d5a1f33c3da2625eda87b42097394
Amaury Martin <amaury.conde@hotmail.com>
2021-09-05 03:32:47 -0300

    Set typeorm on backend

---
+++ backend/ormconfig.ts
@@ -0,0 +1,12 @@
+export default {
+  "type": "postgres",
+  "host": process.env.DB_HOST,
+  "port": 5432,
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PASSWORD,
+  "database": "happy",
+  "migrations": ["./src/db/migrations/*.ts"],
+  "cli": {
+      "migrationsDir": "./src/db/migrations"
+  },
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv';
+
 import app from './app';
+import './db/connection';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
