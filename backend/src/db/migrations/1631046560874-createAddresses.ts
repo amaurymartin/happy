e82b89c4225409e57144c4015c52cf049e1c9ea7
Amaury Martin <amaury.conde@hotmail.com>
2021-09-07 18:20:49 -0300

    Add addresses migration on backend

---
+++ backend/src/db/migrations/1631046560874-createAddresses.ts
@@ -0,0 +1,85 @@
+/* eslint-disable class-methods-use-this */
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+// eslint-disable-next-line import/prefer-default-export
+export class createAddresses1631046560874 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'addresses',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'latitude',
+            type: 'decimal',
+            isNullable: false,
+            precision: 10,
+            scale: 8,
+          },
+          {
+            name: 'longitude',
+            type: 'decimal',
+            isNullable: false,
+            precision: 11,
+            scale: 8,
+          },
+          {
+            name: 'street',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'number',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'complement',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'zip_code',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'city',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'state',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'country',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            isNullable: false,
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('addresses');
+  }
+}
