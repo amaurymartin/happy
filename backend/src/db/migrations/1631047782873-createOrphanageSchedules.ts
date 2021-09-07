5bcebb5fcdb9a56592ac1ab568a06b36d1905a2c
Amaury Martin <amaury.conde@hotmail.com>
2021-09-07 19:50:46 -0300

    Add orphanages_schedules migration on backend

---
+++ backend/src/db/migrations/1631047782873-createOrphanageSchedules.ts
@@ -0,0 +1,72 @@
+/* eslint-disable class-methods-use-this */
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+// eslint-disable-next-line import/prefer-default-export
+export class createOrphanageSchedules1631047782873
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orphanage_schedules',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'orphanage_id',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'week_day',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'starts_at',
+            type: 'time',
+            isNullable: false,
+          },
+          {
+            name: 'ends_at',
+            type: 'time',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            isNullable: false,
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orphanage_schedules',
+      new TableForeignKey({
+        columnNames: ['orphanage_id'],
+        referencedTableName: 'orphanages',
+        referencedColumnNames: ['id'],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orphanage_schedules');
+  }
+}
