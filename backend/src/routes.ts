6c99b25ca3186bfe7de0ff8d9ba8302d49cd060e
Amaury Martin <amaury.conde@hotmail.com>
2021-09-14 03:24:40 -0300

    Add route to upload images on backend

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import multer from 'multer';
+
+import multerConfig from '../multerConfig';
+
 import OrphanagesController from './controllers/orphanagesController';
+import ImagesController from './controllers/orphanages/imagesController';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
 
 // Healthcheck
 routes.get('/healthcheck', (_req, res) => {
@@ -13,5 +19,12 @@ routes.post('/orphanages', OrphanagesController.create);
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:key', OrphanagesController.show);
 routes.put('/orphanages/:key', OrphanagesController.update);
+
+// Orphanages Images
+routes.post(
+  '/orphanages/:key/images',
+  upload.array('images'),
+  ImagesController.create,
+);
 
 export default routes;
