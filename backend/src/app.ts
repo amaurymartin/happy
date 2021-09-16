56ce81fa81325d1fb8ce45b9d10aee9f13675ffd
Amaury Martin <amaury.conde@hotmail.com>
2021-09-16 01:21:24 -0300

    Add route to expose assets on backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+
 import routes from './routes';
 
 const app = express();
@@ -7,5 +9,6 @@ const app = express();
 app.use(cors({ exposedHeaders: 'X-Total-Count' }));
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 export default app;
