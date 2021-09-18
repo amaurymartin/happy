2f4b452e7fc22a072eac01cb7d760fccc753b0d4
Amaury Martin <amaury.conde@hotmail.com>
2021-09-18 04:29:13 -0300

    Add new routes on frontend web

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,12 +1,18 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/home';
 import OrphanagesIndex from './pages/orphanages/index';
+import OrphanageShow from './pages/orphanages/show';
+import OrphanageNew from './pages/orphanages/new';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
-    <Route path="/" exact component={Home} />
-    <Route path="/orphanages" component={OrphanagesIndex} />
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/orphanages" exact component={OrphanagesIndex} />
+      <Route path="/orphanages/new" component={OrphanageNew} />
+      <Route path="/orphanages/:key" component={OrphanageShow} />
+    </Switch>
   </BrowserRouter>
 );
 
