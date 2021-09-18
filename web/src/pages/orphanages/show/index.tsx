b3ab0b3cc3f9b9d46bbcee7ed71c1f0d36670921
Amaury Martin <amaury.conde@hotmail.com>
2021-09-18 04:28:50 -0300

    Init OrphanageShow page on frontend web

---
+++ web/src/pages/orphanages/show/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css';
+
+const OrphanageShow: React.FC = () => {
+  // eslint-disable-next-line no-unused-vars
+  const foo = 'bar';
+
+  return (
+    <div id="orphanage-show">
+      <p>Show Orphanage</p>
+    </div>
+  );
+};
+
+export default OrphanageShow;
