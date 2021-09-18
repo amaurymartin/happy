b0b0e94723f75ee95b5dd6da9421e6cce9aafebe
Amaury Martin <amaury.conde@hotmail.com>
2021-09-18 04:28:37 -0300

    Init OrphanageNew page on frontend web

---
+++ web/src/pages/orphanages/new/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css';
+
+const OrphanageNew: React.FC = () => {
+  // eslint-disable-next-line no-unused-vars
+  const foo = 'bar';
+
+  return (
+    <div id="orphanage-new">
+      <p>New Orphanage</p>
+    </div>
+  );
+};
+
+export default OrphanageNew;
