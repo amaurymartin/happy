815864dbfcb627c4e2be8d9ac29eea5ac54af309
Amaury Martin <amaury.conde@hotmail.com>
2021-09-19 19:44:39 -0300

    Move import leaflet.css to App component on frontend web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,8 @@
-import './assets/styles/global.css';
-
 import Routes from './routes';
+
+import 'leaflet/dist/leaflet.css';
+
+import './assets/styles/global.css';
 
 const App: React.FC = () => <Routes />;
 
