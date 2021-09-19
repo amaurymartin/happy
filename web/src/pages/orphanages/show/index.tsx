f65b4615255b418f0f919c6a9d63f88321fe95d8
Amaury Martin <amaury.conde@hotmail.com>
2021-09-19 04:50:40 -0300

    Improve orphanagesShow page on frontend web

--- web/src/pages/orphanages/show/index.tsx
+++ web/src/pages/orphanages/show/index.tsx
@@ -1,12 +1,143 @@
+import { useState } from 'react';
+
+import { Link, useHistory } from 'react-router-dom';
+
+import { FaWhatsapp } from 'react-icons/fa';
+import { FiClock, FiInfo, FiArrowLeft } from 'react-icons/fi';
+
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import Leaflet from 'leaflet';
+
+import mapMarkerImg from '../../../assets/images/map-marker.svg';
+
 import './styles.css';
 
 const OrphanageShow: React.FC = () => {
   // eslint-disable-next-line no-unused-vars
-  const foo = 'bar';
+  const [currentPosition, setCurrentPosition] = useState<[number, number]>([
+    -3.7436121, -38.5194538,
+  ]);
+
+  const mapMarker = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+    iconSize: [42, 42],
+    popupAnchor: [0, -10],
+  });
+
+  const { goBack } = useHistory();
 
   return (
     <div id="orphanage-show">
-      <p>Show Orphanage</p>
+      <aside>
+        <img src={mapMarkerImg} alt="Happy" />
+
+        <footer>
+          <button type="button" onClick={goBack}>
+            <FiArrowLeft size={24} color="#FFF" />
+          </button>
+        </footer>
+      </aside>
+
+      <main>
+        <div className="orphanage-details">
+          <img
+            src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+            alt="First Orphanage"
+          />
+
+          <div className="images">
+            <button className="active" type="button">
+              <img
+                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+                alt="First Orphanage"
+              />
+            </button>
+            <button type="button">
+              <img
+                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+                alt="First Orphanage"
+              />
+            </button>
+            <button type="button">
+              <img
+                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+                alt="First Orphanage"
+              />
+            </button>
+            <button type="button">
+              <img
+                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+                alt="First Orphanage"
+              />
+            </button>
+            <button type="button">
+              <img
+                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+                alt="First Orphanage"
+              />
+            </button>
+            <button type="button">
+              <img
+                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+                alt="First Orphanage"
+              />
+            </button>
+          </div>
+
+          <div className="orphanage-details-content">
+            <h1>First Orphanage</h1>
+            <p>About First Orphanage</p>
+
+            <div className="map-container">
+              <MapContainer
+                center={currentPosition}
+                zoom={16}
+                style={{ width: '100%', height: 280 }}
+                touchZoom={false}
+                doubleClickZoom={false}
+              >
+                <TileLayer
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker
+                  icon={mapMarker}
+                  position={currentPosition}
+                  interactive={false}
+                />
+              </MapContainer>
+
+              <footer>
+                <Link to="/">See routes on Google Maps</Link>
+              </footer>
+            </div>
+
+            <hr />
+
+            <h2>Instructions to visit</h2>
+            <p>Instructions one, two, and three</p>
+
+            <div className="open-details">
+              <div className="hour">
+                <FiClock size={32} color="#15B6D6" />
+                Monday to Friday
+                <br />
+                8:00 AM to 06:00 PM
+              </div>
+
+              <div className="open-on-weekends">
+                <FiInfo size={32} color="#39CC83" />
+                Open on weekends
+              </div>
+            </div>
+
+            <button type="button" className="contact-button">
+              <FaWhatsapp size={20} color="#FFF" />
+              Contact
+            </button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
