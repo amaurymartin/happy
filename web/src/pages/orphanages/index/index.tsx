09f17b1d7479c47952cd19ec4ddefde1d91e7959
Amaury Martin <amaury.conde@hotmail.com>
2021-09-20 01:07:27 -0300

    Integrate orphanagesIndex page with backend on frontend web

--- web/src/pages/orphanages/index/index.tsx
+++ web/src/pages/orphanages/index/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Link } from 'react-router-dom';
 import { FiArrowRight, FiPlus } from 'react-icons/fi';
@@ -10,13 +10,44 @@ import mapMarkerImg from '../../../assets/images/map-marker.svg';
 
 import mapMarker from '../../../utils/mapMarker';
 
+import Orphanage from '../../../types/entities/orphanage/orphanage';
+import OrphanageIndexResponse from '../../../types/responses/orphanages/index';
+
+import api from '../../../services/api';
+
 import './styles.css';
 
 const OrphanagesIndex: React.FC = () => {
-  // eslint-disable-next-line no-unused-vars
   const [currentPosition, setCurrentPosition] = useState<[number, number]>([
-    -3.7436121, -38.5194538,
+    NaN,
+    NaN,
   ]);
+
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setCurrentPosition([position.coords.latitude, position.coords.longitude]);
+    });
+  }, []);
+
+  useEffect(() => {
+    api
+      .get<OrphanageIndexResponse>('orphanages')
+      .then((response) => setOrphanages(response.data.orphanages))
+      .catch((response) => {
+        // eslint-disable-next-line no-console
+        console.log('response', response);
+        // eslint-disable-next-line no-alert
+        alert('Error on loading orphanages. Please refresh the page');
+      });
+  }, []);
+
+  function coordsLoaded() {
+    return (
+      !Number.isNaN(currentPosition[0]) && !Number.isNaN(currentPosition[1])
+    );
+  }
 
   return (
     <div id="orphanages-index">
@@ -34,30 +65,43 @@ const OrphanagesIndex: React.FC = () => {
         </footer>
       </aside>
 
-      <MapContainer
-        center={currentPosition}
-        zoom={13}
-        style={{ width: '100%', height: '100%' }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+      {coordsLoaded() ? (
+        <MapContainer
+          center={currentPosition}
+          zoom={13}
+          style={{ width: '100%', height: '100%' }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
 
-        <Marker icon={mapMarker} position={currentPosition}>
-          <Popup
-            className="map-popup"
-            closeButton={false}
-            minWidth={240}
-            maxWidth={240}
-          >
-            <p>First Orphanage</p>
-            <Link to="/orphanages/1">
-              <FiArrowRight size={20} color="#000" />
-            </Link>
-          </Popup>
-        </Marker>
-      </MapContainer>
+          {orphanages.map((orphanage) => (
+            <Marker
+              icon={mapMarker}
+              position={[
+                orphanage.address.latitude,
+                orphanage.address.longitude,
+              ]}
+              key={orphanage.key}
+            >
+              <Popup
+                className="map-popup"
+                closeButton={false}
+                minWidth={240}
+                maxWidth={240}
+              >
+                <p>{orphanage.name}</p>
+                <Link to={`/orphanages/${orphanage.key}`}>
+                  <FiArrowRight size={20} color="#000" />
+                </Link>
+              </Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+      ) : (
+        'LOADING MAP'
+      )}
 
       <Link to="/orphanages/new" className="new-orphanage">
         <FiPlus size={32} color="#FFF" />
