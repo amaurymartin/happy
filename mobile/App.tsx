1ec4a65e02b5edee95a78dde53309c6ede37ca92
Amaury Martin <amaury.conde@hotmail.com>
2021-10-02 01:29:15 -0300

    Init OrphanagesIndex page on frontend mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+import OrphanagesIndex from './src/pages/orphanages/index';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+      <OrphanagesIndex />
     </View>
   );
 }
---
+++ mobile/src/pages/orphanages/index/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import styles from './styles';
+
+const OrphanagesIndex: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>OrphanagesIndex</Text>
+    </View>
+  )
+}
+
+export default OrphanagesIndex;
