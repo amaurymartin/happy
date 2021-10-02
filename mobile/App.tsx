418f3d053c2814a9f931b2341b2d51df4b8a0882
Amaury Martin <amaury.conde@hotmail.com>
2021-10-02 02:22:06 -0300

    Add fonts on frontend mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+
 import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+import {
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+} from '@expo-google-fonts/nunito';
+import AppLoading from 'expo-app-loading';
 
 import OrphanagesIndex from './src/pages/orphanages/index';
 
-export default function App() {
+export default function App(): JSX.Element {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
