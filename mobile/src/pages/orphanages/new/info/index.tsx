804b5c457ebda015a96a3af598951b5fec97648d
Amaury Martin <amaury.conde@hotmail.com>
2021-10-03 03:21:03 -0300

    Improve orphanagesNew page on frontend mobile

--- mobile/src/pages/orphanages/new/info/index.tsx
+++ mobile/src/pages/orphanages/new/info/index.tsx
@@ -1,14 +1,61 @@
 import React from 'react';
 
-import { Text, View } from 'react-native';
+import {
+  ScrollView,
+  Switch,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import { Feather } from '@expo/vector-icons';
 
 import styles from './styles';
 
 const OrphanageNew: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>OrphanageNew</Text>
-    </View>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={{ padding: 24 }}
+    >
+      <Text style={styles.title}>Info</Text>
+
+      <Text style={styles.label}>Name</Text>
+      <TextInput style={styles.input} />
+
+      <Text style={styles.label}>About</Text>
+      <TextInput style={[styles.input, { height: 110 }]} multiline />
+
+      <Text style={styles.label}>Whatsapp</Text>
+      <TextInput style={styles.input} />
+
+      <Text style={styles.label}>Images</Text>
+      <TouchableOpacity style={styles.imagesInput} onPress={() => {}}>
+        <Feather name="plus" size={24} color="#15b6d6" />
+      </TouchableOpacity>
+
+      <Text style={styles.title}>Visiting</Text>
+
+      <Text style={styles.label}>Instructions</Text>
+      <TextInput style={[styles.input, { height: 110 }]} multiline />
+
+      <Text style={styles.label}>Schedule</Text>
+      <TextInput style={styles.input} />
+
+      <View style={styles.switchContainer}>
+        <Text style={styles.label}>Open on weekends</Text>
+        <Switch
+          thumbColor="#fff"
+          trackColor={{ false: '#ccc', true: '#39cc83' }}
+        />
+      </View>
+
+      <RectButton style={styles.nextButton} onPress={() => {}}>
+        <Text style={styles.nextButtonText}>Create</Text>
+      </RectButton>
+    </ScrollView>
   );
 };
 
--- mobile/src/pages/orphanages/new/info/styles.ts
+++ mobile/src/pages/orphanages/new/info/styles.ts
@@ -4,6 +4,73 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  title: {
+    marginBottom: 32,
+    paddingBottom: 24,
+    borderBottomWidth: 0.8,
+    borderBottomColor: '#d3e2e6',
+
+    fontSize: 24,
+    fontFamily: 'Nunito_700Bold',
+    color: '#5c8599',
+  },
+  label: {
+    marginBottom: 8,
+    fontFamily: 'Nunito_600SemiBold',
+    color: '#8fa7b3',
+  },
+  comment: {
+    fontSize: 11,
+    color: '#8fa7b3',
+  },
+  input: {
+    height: 56,
+    marginBottom: 16,
+    paddingVertical: 18,
+    paddingHorizontal: 24,
+    borderRadius: 20,
+    borderWidth: 1.4,
+
+    borderColor: '#d3e2e6',
+    backgroundColor: '#fff',
+
+    textAlignVertical: 'top',
+  },
+  imagesInput: {
+    height: 56,
+    marginBottom: 32,
+    borderRadius: 20,
+    borderWidth: 1.4,
+    borderStyle: 'dashed',
+
+    borderColor: '#96d2f0',
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  switchContainer: {
+    marginTop: 16,
+
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  nextButton: {
+    height: 56,
+    marginTop: 32,
+    borderRadius: 20,
+
+    backgroundColor: '#15c3d6',
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  nextButtonText: {
+    fontFamily: 'Nunito_800ExtraBold',
+    fontSize: 16,
+    color: '#fff',
+  },
 });
 
 export default styles;
