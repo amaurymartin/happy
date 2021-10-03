b7a46bfd78bbcb36eff5fe9eee818e30d14a9a0b
Amaury Martin <amaury.conde@hotmail.com>
2021-10-03 02:43:43 -0300

    Improve orphanagesShow page on frontend mobile

--- mobile/src/pages/orphanages/show/index.tsx
+++ mobile/src/pages/orphanages/show/index.tsx
@@ -1,14 +1,98 @@
 import React from 'react';
 
-import { Text, View } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import { Feather, FontAwesome } from '@expo/vector-icons';
+
+import mapMarker from '../../../assets/map/marker.png';
 
 import styles from './styles';
 
 const OrphanagesShow: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>OrphanagesShow</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.imagesContainer}>
+        <ScrollView horizontal pagingEnabled>
+          <Image
+            style={styles.image}
+            source={{
+              uri: 'https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg',
+            }}
+          />
+          <Image
+            style={styles.image}
+            source={{
+              uri: 'https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg',
+            }}
+          />
+          <Image
+            style={styles.image}
+            source={{
+              uri: 'https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg',
+            }}
+          />
+        </ScrollView>
+      </View>
+
+      <View style={styles.detailsContainer}>
+        <Text style={styles.title}>Orphanage name</Text>
+        <Text style={styles.description}>About Orphanage</Text>
+
+        <View style={styles.mapContainer}>
+          <MapView
+            style={styles.map}
+            provider={PROVIDER_GOOGLE}
+            initialRegion={{
+              latitude: -3.7436121,
+              longitude: -38.5194538,
+              latitudeDelta: 0.033,
+              longitudeDelta: 0.033,
+            }}
+            pitchEnabled={false}
+          >
+            <Marker
+              icon={mapMarker}
+              coordinate={{
+                latitude: -3.7436121,
+                longitude: -38.5194538,
+              }}
+            />
+          </MapView>
+        </View>
+
+        <View style={styles.routesContainer}>
+          <Text style={styles.routesText}>See routes on Google Maps</Text>
+        </View>
+
+        <View style={styles.separator} />
+
+        <Text style={styles.title}>Instructions to visit</Text>
+        <Text style={styles.description}>Orphanage instructions</Text>
+
+        <View style={styles.scheduleContainer}>
+          <View style={[styles.scheduleItem, styles.scheduleItemBlue]}>
+            <Feather name="clock" size={40} color="#2ab5d1" />
+            <Text style={[styles.scheduleText, styles.scheduleTextBlue]}>
+              Monday to Friday 8:00 AM to 06:00 PM
+            </Text>
+          </View>
+
+          <View style={[styles.scheduleItem, styles.scheduleItemGreen]}>
+            <Feather name="info" size={40} color="#39cc83" />
+            <Text style={[styles.scheduleText, styles.scheduleTextGreen]}>
+              Open on weekends
+            </Text>
+          </View>
+        </View>
+
+        <RectButton style={styles.contactButton} onPress={() => {}}>
+          <FontAwesome name="whatsapp" size={24} color="#fff" />
+          <Text style={styles.contactButtonText}>Contact</Text>
+        </RectButton>
+      </View>
+    </ScrollView>
   );
 };
 
--- mobile/src/pages/orphanages/show/styles.ts
+++ mobile/src/pages/orphanages/show/styles.ts
@@ -1,8 +1,119 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  imagesContainer: {
+    height: 240,
+  },
+  image: {
+    width: Dimensions.get('window').width,
+    height: 240,
+    resizeMode: 'cover',
+  },
+  detailsContainer: {
+    padding: 24,
+  },
+  title: {
+    fontFamily: 'Nunito_700Bold',
+    fontSize: 30,
+    color: '#4d6f80',
+  },
+  description: {
+    marginTop: 16,
+    lineHeight: 24,
+
+    fontFamily: 'Nunito_600SemiBold',
+    color: '#5c8599',
+  },
+  mapContainer: {
+    marginTop: 40,
+    overflow: 'hidden',
+
+    borderRadius: 20,
+    borderWidth: 1.2,
+
+    borderColor: '#b3dae2',
+    backgroundColor: '#e6f7fb',
+  },
+  map: {
+    width: '100%',
+    height: 150,
+  },
+  routesContainer: {
+    padding: 16,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  routesText: {
+    fontFamily: 'Nunito_700Bold',
+    color: '#0089a5',
+  },
+  separator: {
+    width: '100%',
+    height: 0.8,
+
+    marginVertical: 40,
+
+    backgroundColor: '#d3e2e6',
+  },
+  scheduleContainer: {
+    marginTop: 24,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  scheduleItem: {
+    width: '48%',
+    padding: 20,
+  },
+  scheduleItemBlue: {
+    borderRadius: 20,
+    borderWidth: 1,
+
+    borderColor: '#b3dae2',
+    backgroundColor: '#e6f7fb',
+  },
+
+  scheduleItemGreen: {
+    borderRadius: 20,
+    borderWidth: 1,
+
+    borderColor: '#a1e9c5',
+    backgroundColor: '#edfff6',
+  },
+  scheduleText: {
+    marginTop: 20,
+    lineHeight: 24,
+
+    fontFamily: 'Nunito_600SemiBold',
+    fontSize: 16,
+  },
+  scheduleTextBlue: {
+    color: '#5c8599',
+  },
+  scheduleTextGreen: {
+    color: '#37c77f',
+  },
+  contactButton: {
+    height: 56,
+    marginTop: 40,
+    borderRadius: 20,
+
+    backgroundColor: '#3cdc8c',
+
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  contactButtonText: {
+    marginLeft: 16,
+
+    fontFamily: 'Nunito_800ExtraBold',
+    fontSize: 16,
+    color: '#fff',
   },
 });
 
